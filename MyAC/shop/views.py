52445e3a0416a1ec320de29eb9e6910da50cf3f8
yash rastogi <yashrastogi201@gmail.com>
2021-02-07 13:14:13 +0530

    Initial commit

---
+++ MyAC/shop/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
+
+# Create your views here.
+def index(request):
+    #products = Product.objects.all()
+    #print(products)
+    #n = len(products)
+
+    #print(n)
+    #nSlides = n // 4 + ceil((n / 4) - (n // 4))
+    #params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
+    #allProds=[[products ,  range(1, nSlides) , nSlides] , [products ,  range(1, nSlides) , nSlides]]
+    allProds = []
+    catProds = Product.objects.values('category','id')
+    print(catProds)
+    cats = {item['category'] for item in catProds}
+    print(cats)
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod,range(1,nSlides),nSlides])
+
+
+    params={"allProds":allProds}
+    return render(request, 'shop/index.html', params)
+
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+
+def search(request):
+    return render(request, 'shop/search.html')
+
+
+def productView(request,myid):
+    # fetch the product using id
+    product = Product.objects.filter(id = myid)
+    print(product)
+    return render(request, 'shop/prodView.html',{'product':product[0]})
+
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
